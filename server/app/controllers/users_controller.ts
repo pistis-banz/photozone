d6ed315b157d19e40cc98228eb62651bec5aca4f
pistisbanz <pistisbasolua@gmail.com>
2025-07-20 18:20:46 +0100

    mise à jour du système de sauvegarde de l'avatar

--- server/app/controllers/users_controller.ts
+++ server/app/controllers/users_controller.ts
@@ -1,49 +1,74 @@
 
 import { createUserValidator, loginValidator } from '#validators/user'
+import { cuid } from '@adonisjs/core/helpers'
 import type { HttpContext } from '@adonisjs/core/http'
 import logger from '@adonisjs/core/services/logger'
-import { cuid } from '@adonisjs/core/helpers'
+import drive from '@adonisjs/drive/services/main'
+import sharp from 'sharp'
 
 
 export default class UsersController {
     public async createUser({request, response}: HttpContext) {
         let avatar: any = null
         let payload: any
-
+    
         try {
             payload = await request.validateUsing(createUserValidator)
             logger.info("payload is valid")
 
-            
             const avatarFile = request.file('avatar', {
-                size: '2mb',
+                size: '5mb',
                 extnames: ['jpeg', 'jpg', 'png'],
               })
               if (!avatarFile) {
                 return response.badRequest({ error: 'Image missing' })
               }
               avatar = avatarFile
+              
+              const userId = cuid();
 
-              const key = `uploads/pistisbanz/${cuid()}.${avatarFile.extname}`
-              await avatarFile.moveToDisk(key)
+              // creation des chemins pour l'avatar 
 
-              console.log("avatar", avatarFile.meta.url)
+              const thumbnailPath = `uploads/users/${userId}/avatar/thumbnail/${cuid()}.${avatarFile.extname}`
+              const avatarPath = `uploads/users/${userId}/avatar/original/${cuid()}.${avatarFile.extname}`
+
+              /* modification des dimensions de l'avatar avec sharp  */
+            
+              const thumbnail = avatarFile.tmpPath
+      ? sharp(avatarFile.tmpPath)
+          .resize(300, 300) // redimensionner en 300x300
+          .toFormat('jpeg')
+          .jpeg({ quality: 80 })
+          .toBuffer()
+      :  null
+      
+      if (!thumbnail) {
+        return response.badRequest({ error: 'Impossible de traiter l\'image' })
+      }
+      
+
+
+      // Sauvegarder le buffer avec Drive
+      const disk = drive.use()
+      await disk.put(thumbnailPath, await thumbnail)
+      await avatarFile.moveToDisk(avatarPath)
 
         } catch (error) {
-            console.log(error.message)
-          return response.status(400).send(error.messages)
+            console.log(error)
+          return response.status(400).send(error)
         }
      
-        return response.status(500).send(payload)
+        return response.status(201).send(payload)
     }
+
     public async login({request, response}: HttpContext) {
         let payload: any
         try {
             payload = await request.validateUsing(loginValidator)
             logger.info("payload is valid")
         } catch (error) {
-            console.log(error.message)
-          return response.status(400).send(error.messages)
+            console.log(error)
+          return response.status(400).send(error)
         }
         return response.status(200).send(payload)
     }
